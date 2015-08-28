3105af8cac58f745c3bf312f3d682c8e1388d5ca
Oded Sharon <git@odedsharon.com>
2015-08-28 18:06:53 +0300

    lots of progress

---
+++ Assets/CHageAfterLoad.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class CHageAfterLoad : MonoBehaviour {
+
+	public Text SavingChild;
+	public Text Scanning;
+	// Use this for initialization
+	void Start () {
+	
+		StartCoroutine(runAnims());
+	}
+
+
+	IEnumerator runAnims() {
+		yield return new WaitForSeconds(1);
+		SavingChild.text = "Saving Child.";
+		yield return new WaitForSeconds(1);
+		SavingChild.text = "Saving Child..";
+		yield return new WaitForSeconds(1);
+		SavingChild.text = "Saving Child...";
+		Scanning.gameObject.SetActive (true);
+		Scanning.enabled = true;
+		yield return new WaitForSeconds(1);
+		Scanning.text = "Scanning For Viruses.";
+		yield return new WaitForSeconds(1);
+		Scanning.text = "Scanning For Viruses..";
+		yield return new WaitForSeconds(1);
+		Scanning.text = "Scanning For Viruses...";
+		Application.LoadLevel ("Congratulations");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Logic.cs
+++ Assets/Logic.cs
@@ -9,16 +9,25 @@ public class Logic : MonoBehaviour {
 	static int KidsSaved=0;
 	public string [] KidNames;
 
+	public Sprite [] sprites;
+
 	public Text kidnameText;
+	public Image kidImage;
 
 	// Use this for initialization
 	void Start () {
 
 
-		if (Application.loadedLevelName == "ThankYou") {
+		if (Application.loadedLevelName == "ThankYou"||
+		    Application.loadedLevelName == "Congratulations") {
 			kidnameText.text = KidNames[i];
 		}
 
+
+		if ( Application.loadedLevelName == "Congratulations") {
+			kidImage.sprite = sprites[i];
+			kidnameText.text = KidNames[i];
+		}
 		
 	}
 	
@@ -43,9 +52,38 @@ public class Logic : MonoBehaviour {
 		Kids [i].GetComponent<Animator> ().Play ("MoveImage 2");
 		i++;
 
+		if (i == 5) {
+			i=0;
+			foreach (RectTransform k in Kids) {
+				k.GetComponent<Animator>().Play("MoveImage");
+			}
+
+		}
 	}
 
 	public void ClickStart() {
 		Application.LoadLevel ("MainScene");
 	}
+
+	public void ClickSave() {
+		Application.LoadLevel ("Saving");
+	}
+
+	
+	public void ClickOK() {
+		//Application.Quit(); 
+			Application.LoadLevel ("NowDoSomething");
+	}
+
+	public void ClickQuit() {
+			Application.Quit(); 
+
+	}
+
+	
+	public void ClickShare() {
+		Application.OpenURL ("");
+		
+	}
+
 }
